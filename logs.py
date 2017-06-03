4e64f07ec57c82614bdfa3d5fb97bded3f9d168a
J.T. Robinson <jaittarius@hotmail.com>
2017-06-02 21:17:13 -0400

    Initial app layout

--- logs.py
+++ logs.py
@@ -5,17 +5,13 @@
 
 from flask import Flask, request, redirect, url_for
 
-from forumdb import get_posts, add_post
+from logsdb import get_posts, add_post
 
 app = Flask(__name__)
 
 
 @app.route('/', methods=['GET'])
 def main():
-
-
-@app.route('/', methods=['POST'])
-def post():
 
 
 if __name__ == '__main__':
